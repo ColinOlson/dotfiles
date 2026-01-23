496f7310d1a0a72c48327220370a7b80d964bd37
Colin Olson <thecolin@gmail.com>
2026-01-23 13:02:23 -0800

    Home manager

--- flake.nix
+++ flake.nix
@@ -3,22 +3,35 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs }: 
+  outputs = { self, nixpkgs, home-manager }: 
     let 
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
+      pkgs = nixpkgs.legacyPackages.${system};
     in
     {
       nixosConfigurations."nixos-lappy" = nixpkgs.lib.nixosSystem {
         inherit system;
+
         specialArgs = {
           hostname = "nixos-lappy";
         };
+
         modules = [
           ./configuration.nix
         ];
+      };
+
+      homeConfigurations.colino = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        modules = [
+          ./home.nix
+        ];
+
       };
     };
 }
