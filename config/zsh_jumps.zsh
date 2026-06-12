19ca5c51184db5114e4824657ce6c989f8ddecf7
Colin Olson <thecolin@gmail.com>
2026-06-12 12:08:43 -0700

    Session jump

--- config/zsh_jumps.zsh
+++ config/zsh_jumps.zsh
@@ -81,3 +81,24 @@ tmuxp_project() {
 
 bindkey -s '^G' 'jump\n'
 bindkey -s '^X^P' 'tmuxp_project\n'
+
+
+
+
+
+function sesh-sessions() {
+  {
+    exec </dev/tty
+    exec <&1
+    local session
+    session=$(sesh list -t -c | fzf --height 40% --reverse --border-label ' sesh ' --border --prompt '⚡  ')
+    zle reset-prompt > /dev/null 2>&1 || true
+    [[ -z "$session" ]] && return
+    sesh connect $session
+  }
+}
+
+zle     -N             sesh-sessions
+bindkey -M emacs '\es' sesh-sessions
+bindkey -M vicmd '\es' sesh-sessions
+bindkey -M viins '\es' sesh-sessions
